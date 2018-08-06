ecff8d26a93f03283d59463b7159975bc56209e6
jiangjiechu <jiangjiechu@163.com>
2018-08-06 21:38:48 +0800

    08062137
    完成了正负样本到正样本中心距离的计算

--- HOG/ext/test.py
+++ HOG/ext/test.py
@@ -18,28 +18,28 @@ test_pic = fun.GetOneFrame(packed_img,size_y,size_x,329,n_col)
 # h = h.reshape((n_cells,n_cells,9))
 # h_img = fun.HOG_pic_cv2(test_pic,h)
 
+#采集正样本
 f_positive = open(home+'result/video1_pos.txt','r')
 s_pos = []
 for entry in f_positive:
     f_beg,f_end = list(map(int,entry.split()))
-    # print("%d~%d is processing:\n"%(f_beg,f_end))
     for i in range(f_beg,f_end+1):
         print("%d\t"%i)
         s_pos.append(fun.GetOneFrame(packed_img,size_y,size_x,i,n_col))
     print("\n")
-
 f_positive.close()
 
-# hog_pos = []
-# start_time = datetime.datetime.now()
-# for pic in s_pos:
-    # hog_pos.append(fun.HOGCalc(pic,8,9))
+#计算正样本的hog
+hog_pos = []
+start_time = datetime.datetime.now()
+for pic in s_pos:
+    hog_pos.append(fun.HOGCalc(pic,8,9))
+end_time = datetime.datetime.now()
+print((end_time-start_time).seconds)
+hog_pos = np.array(hog_pos)
 
-# end_time = datetime.datetime.now()
-# print((end_time-start_time).seconds)
-# hog_pos = np.array(hog_pos)
-
-
+#提取负样本,暂定从f_beg向后取5~50内的帧,可形成405~4050个大小的负样本集合
+#用neg_per_shot控制一次假进球帧后面采集多少负样本,neg_per_shot越大,静止帧数越多
 f_neg = open(home+'result/video1_neg.txt','r')
 s_neg = []
 for entry in f_neg:
@@ -49,16 +49,26 @@ for entry in f_neg:
         print("%d\t"%i)
         s_neg.append(fun.GetOneFrame(packed_img,size_y,size_x,i,n_col))
     print("\n")
-
 f_neg.close()
 total_neg = len(s_neg)
 print(total_neg)
 
+#计算负样本hog
 hog_neg = []
 start_time = datetime.datetime.now()
 for pic in s_neg:
     hog_neg.append(fun.HOGCalc(pic,8,9))
-
 end_time = datetime.datetime.now()
 print((end_time-start_time).seconds)
-hog_pos = np.array(hog_neg)
+hog_neg = np.array(hog_neg)
+
+#计算正样本中心center_pos
+center_pos = sum(hog_pos)/hog_pos.shape[0]
+
+#计算正负样本到center的距离
+dist_pos = np.zeros((hog_pos.shape[0]))
+for hog in hog_pos:
+    dist_pos = np.sqrt((hog-center_pos)**2)
+dist_neg = np.zeros((hog_neg.shape[0]))
+for hog in hog_neg:
+    dist_neg = np.sqrt((hog-center_pos)**2)
